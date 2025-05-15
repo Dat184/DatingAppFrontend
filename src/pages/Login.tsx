703cd5fd99176a7a7f09f92c3b2f739cf17ec9fa
Dat184 <thanhnguyendat184@gmail.com>
2025-05-15 16:33:57 +0700

    done login with gg page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import BgLogin from "../assets/img/Bg_Login.jpg";
 import IconGoogle from "../assets/img/Icon_Google.png";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+
 const Login = () => {
-  const handleBtnLogin = () => {
-    alert("Login with Google");
-  };
+  const navigate = useNavigate();
+
+  const handleLogin = useGoogleLogin({
+    onSuccess: async (credentialResponse) => {
+      try {
+        const res = await axios.get(
+          `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${credentialResponse.access_token}`
+        );
+        console.log(res.data);
+        navigate("/");
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    },
+    onError: (error) => {
+      console.error("Login Failed:", error);
+    },
+  });
+
   return (
     <section className="h-screen relative flex items-center justify-center overflow-hidden">
       <div
@@ -16,12 +36,12 @@ const Login = () => {
       />
       <div className="relative z-10 h-3/4 w-3/5 bg-white/30 flex items-center justify-center rounded-[4.375rem] flex-col ">
         <h1 className="text-[50px] text-black font-normal">PerfectDate</h1>
-        <span className="text[30px] text-black font-thin">
+        <span className="text[30px] text-black font-thin ">
           Less Planning, More Loving
         </span>
         <button
-          className="mt-5 h-8 w-[12rem] bg-white flex items-center justify-center gap-1"
-          onClick={handleBtnLogin}
+          className="mt-[2.5rem] h-8 w-[12rem] bg-white flex items-center justify-center gap-1"
+          onClick={() => handleLogin()}
         >
           <img src={IconGoogle} alt="" className="w-5" />
           <span className="text-[14px] text-black font-thin">
